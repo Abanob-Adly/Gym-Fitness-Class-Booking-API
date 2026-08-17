2fb099cf2765b75ccffc0cc13e3e3e83abb1318b
Abanob Adly <abanob582006@gmail.com>
2026-08-17 21:49:54 +0300

    feat(auth): implement register and login service logic

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,6 +1,7 @@
-import { Schema, model } from "mongoose";
+import { Document, Schema, model } from "mongoose";
+import { minLength } from "zod";
 
-interface IUser {
+interface IUser extends Document {
   name: string;
   email: string;
   password?: string;
@@ -24,6 +25,7 @@ const userSchema = new Schema({
     type: String,
     required: true,
     select: false,
+    minLength: 8,
   },
   role: {
     type: String,
